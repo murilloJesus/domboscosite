337a2b03de8da0718d3c0892f9c9c4799580d76d
Murillo Jesus <murillosjesus@hotmail.com>
2021-09-16 11:52:40 -0300

    update links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,19 +10,19 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
         <link rel="icon" type="image/png" href="/images/icon.png" />
-		<link rel="stylesheet" href="/public/assets/css/main.css" />
-		<link rel="stylesheet" href="/public/assets/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="/assets/css/main.css" />
+		<link rel="stylesheet" href="/assets/css/bootstrap.min.css" />
 
-        <script src="/public/assets/js/jquery.min.js"></script>
-        <script src="/public/assets/js/jquery.dropotron.min.js"></script>
-        <script src="/public/assets/js/jquery.scrolly.min.js"></script>
-        <script src="/public/assets/js/browser.min.js"></script>
-        <script src="/public/assets/js/breakpoints.min.js"></script>
-        <script src="/public/assets/js/util.js"></script>
-        <script src="/public/assets/js/bootstrap.min.js"></script>
-        <script src="/public/assets/js/main.js"></script>
+        <script src="/assets/js/jquery.min.js"></script>
+        <script src="/assets/js/jquery.dropotron.min.js"></script>
+        <script src="/assets/js/jquery.scrolly.min.js"></script>
+        <script src="/assets/js/browser.min.js"></script>
+        <script src="/assets/js/breakpoints.min.js"></script>
+        <script src="/assets/js/util.js"></script>
+        <script src="/assets/js/bootstrap.min.js"></script>
+        <script src="/assets/js/main.js"></script>
 
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="{{ asset('/js/app.js') }}" defer></script>
 	</head>
 	<body class="homepage is-preload" id="app">
 		<div id="page-wrapper" >
