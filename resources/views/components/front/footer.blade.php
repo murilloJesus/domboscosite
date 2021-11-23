ab7cfd8e104f31e0f8351435601466a4060e9098
Murillo Jesus <murillosjesus@hotmail.com>
2021-11-23 17:34:02 -0300

    commit css ajustes

--- resources/views/components/front/footer.blade.php
+++ resources/views/components/front/footer.blade.php
@@ -8,7 +8,7 @@
                                 <div class="col-8 col-12-medium">
                                     <h2 class="major text-start"><span>Mapa do site</span></h2>
                                     <div class="row">
-                                        <div class="col-6 col-12-medium">
+                                        <div class="col-6">
                                             <div class="site-map">
                                                 <a href="/" class="first-level">Home</a>
                                                 <a href="/colegio" class="first-level">Colégio</a>
@@ -20,7 +20,7 @@
                                                     <a href="/colegio#estrutura" class="second-level">Estrutura</a>
                                             </div>
                                         </div>
-                                        <div class="col-6 col-12-medium">
+                                        <div class="col-6">
                                             <div class="site-map">
                                                 <a href="/segmentos" class="first-level">Segmentos</a>
                                                     <a href="/segmentos#infantil" class="second-level">Ensino Infantil</a>
